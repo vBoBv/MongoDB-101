6cd450e89474c8dc6cc15aff31146d1df3300928
vBoBv <vannponhvath@gmail.com>
2020-02-22 23:37:52 +1300

    added test for reading user out of db

--- test/create_test.js
+++ test/create_test.js
@@ -2,11 +2,14 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-	it('saves a user', () => {
+	it('saves a user', done => {
 		// assert(1 + 1 === 3);
 
 		const joe = new User({ name: 'Joe' });
 
-		joe.save();
+		joe.save().then(() => {
+			assert(!joe.isNew);
+			done();
+		});
 	});
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-	.once('open', () => console.log('Good to go!'))
-	.on('error', error => {
-		console.warn('Warning', error);
+mongoose.Promise = global.Promise;
+
+before(done => {
+	mongoose.connect('mongodb://localhost/users_test');
+	mongoose.connection
+		// .once('open', () => console.log('Good to go!'))
+		.once('open', () => done())
+		.on('error', error => {
+			console.warn('Warning', error);
+		});
+});
+
+// mongoose.connect('mongodb://localhost/users_test');
+// mongoose.connection
+// 	.once('open', () => console.log('Good to go!'))
+// 	.on('error', error => {
+// 		console.warn('Warning', error);
+// 	});
+
+beforeEach(done => {
+	mongoose.connection.collections.users.drop(() => {
+		//Ready to run the next test!
+		done();
 	});
+});
