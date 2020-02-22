f468f0f761d7119bafb61386491aa2ccee203106
vBoBv <vannponhvath@gmail.com>
2020-02-23 00:43:40 +1300

    added test for updating users in db

---
+++ test/update_test.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+	let joe;
+
+	beforeEach(done => {
+		joe = new User({ name: 'Joe' });
+		joe.save().then(() => done());
+	});
+
+	assertName = (operation, done) => {
+		operation
+			.then(() => User.find({}))
+			.then(users => {
+				assert(users.length === 1);
+				assert(users[0].name === 'Alex');
+				done();
+			});
+	};
+
+	it('instance type using set and save', done => {
+		// console.log(joe);
+		joe.set('name', 'Alex');
+		// console.log(joe);
+		// joe
+		// 	.save()
+		// 	.then(() => User.find({}))
+		// 	.then(users => {
+		// 		assert(users.length === 1);
+		// 		assert(users[0].name === 'Alex');
+		// 		done();
+		// 	});
+		assertName(joe.save(), done);
+	});
+
+	it('A model instance can update', done => {
+		// joe.update({ name: 'Alex' });
+		assertName(joe.update({ name: 'Alex' }), done);
+	});
+
+	it('A model class can update', done => {
+		assertName(User.update({ name: 'Joe' }, { name: 'Alex' }), done);
+	});
+
+	it('A model class can update one record', done => {
+		assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done);
+	});
+
+	it('A model class can find a record with an Id and update', done => {
+		assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+	});
+});
