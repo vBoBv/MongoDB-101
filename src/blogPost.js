0e8ac71202d0bbed5d0ef5d937610daba1b85bba
vBoBv <vannponhvath@gmail.com>
2020-03-12 22:56:47 +1300

    added association to blogPost and comment

--- src/blogPost.js
+++ src/blogPost.js
@@ -3,5 +3,10 @@ const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
 	title: String,
-	content: String
+	content: String,
+	comments: [{ type: Schema.Types.ObjectId, ref: 'comment' }]
 });
+
+const BlogPost = mongoose.model('blogPost', BlogPostSchema);
+
+module.exports = BlogPost;
