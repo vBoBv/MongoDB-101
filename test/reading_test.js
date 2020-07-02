c11f9a94af3d2a86516f83cf0f2473f4b785cb9e
vBoBv <vannponhvath@gmail.com>
2020-07-02 19:33:37 +1200

    added pagination

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,24 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of database', () => {
-	let joe;
+	// let joe;
+	// beforeEach((done) => {
+	// 	joe = new User({ name: 'Joe' });
+
+	// 	joe.save().then(() => done());
+	// });
+
+	let joe, maria, alex, zach;
 
 	beforeEach((done) => {
 		joe = new User({ name: 'Joe' });
-		joe.save().then(() => done());
+		alex = new User({ name: 'Alex' });
+		maria = new User({ name: 'Maria' });
+		zach = new User({ name: 'Zach' });
+
+		Promise.all([joe.save(), alex.save(), maria.save(), zach.save()]).then(() =>
+			done()
+		);
 	});
 
 	it('finds all users with a name of joe', (done) => {
@@ -27,5 +40,19 @@ describe('Reading users out of database', () => {
 			assert(user.name === 'Joe');
 			done();
 		});
+	});
+
+	it('can skip and limit the result set', () => {
+		//We find all the user inside our user collection, we skipped joe, then we limit only to have alex and maria
+		User.find({})
+			.sort({ name: 1 })
+			.skip(1)
+			.limit(2)
+			.then((users) => {
+				assert(users.length === 2);
+				assert(users[0].name === 'Joe');
+				assert(users[1].name === 'Maria');
+				done();
+			});
 	});
 });
